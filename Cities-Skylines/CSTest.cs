e6a30c4b463088b706bb1d0cb4629ebf7f199200
Cadett <targ1140@icloud.com>
2016-01-11 21:24:31 -0500

    Update CSTest.cs

--- Cities-Skylines/CSTest.cs
+++ Cities-Skylines/CSTest.cs
@@ -1,3 +1,14 @@
-class Test{
-
+using System;
+//using git;
+//using Forms;
+partial class Test{
+  public static void doDatTest(string str){
+    MessageBox.show("Hai! This is a TEST BAYBEH!!");
+  }
+  public static string bahHumbug(){
+    return "bah humbug";
+  }
+  private string disBePrivate(){
+    return "This is private. How did you get here again?";
+  }
 }
