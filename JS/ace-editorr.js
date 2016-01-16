24213d3d45d5fdd29da6a799f9e37d82f2563a2f
Caleb Mayhew <targ1140@icloud.com>
2016-01-16 09:53:04 -0500

    Update and rename ace-editor.2.0.a.js to ace-editorr.js

---
+++ JS/ace-editorr.js
@@ -0,0 +1,4 @@
+window.setInterval(function(){
+  var valueAtAnyTime = "<!DOCTYPE html><html><head><title>Processing.JS inside Webpages: Template</title></head><body><p align=\"center\"> <canvas id=\"mycanvas\"></canvas></p></body> <script src=\"https://cdn.jsdelivr.net/processing.js/1.4.8/processing.min.js\"></script> <script>var sketchProc=function(processingInstance){with(processingInstance){size(400,400);frameRate(30);background(255);" + editor.getValue() + "}};var canvas=document.getElementById(\"mycanvas\");var processingInstance=new Processing(canvas,sketchProc);</script> </html>"
+ $("#tehiframe").attr("srcdoc", valueAtAnyTime);
+ }, 50);
