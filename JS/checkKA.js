cd7562f62fada4c0b35d8ceab5386169faf25cd7
Caleb Mayhew <targ1140@icloud.com>
2016-02-07 15:38:26 -0500

    Create checkKA.js

---
+++ JS/checkKA.js
@@ -0,0 +1,18 @@
+$("check-KA").each(function(){
+  $(this)
+    .css("background", "#5CB85C")
+    .css("color", "white")
+    .css("padding", "1em")
+    .css("position", "fixed")
+    .css("top", "141px")
+    .css("right", "-107px")
+    .css("width", "107px")
+    .css("height", "72px")
+    .css("border-top-left-radius", "1em")
+    .css("border-bottom-left-radius", "1em");
+  $(this).html("<a href='" + $(this).attr("href") + "'>Check me out on KA!</a>");
+  $(this).attr("href", "none");
+  $(this).animate({
+    right: 0
+  }, 1000);
+})
